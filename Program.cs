6814a90699c122e7fdb629e5393739e2e0841f98
Naison <getnais@gmail.com>
2025-05-01 17:10:40 -0400

    Added Url label

--- Program.cs
+++ Program.cs
@@ -261,7 +261,7 @@ async Task RegisterSchemaAsync(bool isUpdate = false)
                 new Property { Name = "type", Type = PropertyType.String, IsQueryable = true, IsSearchable = true, IsRetrievable = true, IsRefinable = false },
                 new Property { Name = "categories", Type = PropertyType.String, IsQueryable = true, IsSearchable = true, IsRetrievable = true, IsRefinable = false },
                 new Property { Name = "lastPublishedAt", Type = PropertyType.String, IsQueryable = false, IsSearchable = false, IsRetrievable = false, IsRefinable = false },
-                new Property { Name = "documentUrl", Type = PropertyType.String, IsQueryable = true, IsSearchable = true, IsRetrievable = true, IsRefinable = false }
+                new Property { Name = "documentUrl", Type = PropertyType.String, IsQueryable = true, IsSearchable = true, IsRetrievable = true, IsRefinable = false, Labels = new List<Label?>(){ Label.Url} }
             },
         };
 
